f1cda30958ee34f0368fb9fab2e2c7f1b6a80ea3
Eduardo Pasqualetti <49802002810@info.local>
2024-04-11 10:46:14 -0300

    envio de email na api

--- WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f03b39bff83c61b946f61634c9810bfe4c0121")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6b364cc38062c6706e7371151de67b61a7c668")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
