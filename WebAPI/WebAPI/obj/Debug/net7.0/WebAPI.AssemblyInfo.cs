fa8c9baccee9b48e01650909993b70c60e27abec
Eduardo Pasqualetti <49802002810@info.local>
2024-05-20 08:06:18 -0300

    Projeto finalizado

--- WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a934eab97dbfe96ffc352b150cb5dde7b681a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee834f97f22fa9d41016c188a9b9ea79025703e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
